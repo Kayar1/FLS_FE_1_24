ea5403e43be2a0b6d66147648598d3e00ec09e55
Kayar <kayar77@gmail.com>
2024-06-24 21:25:30 +0300

    Add files via upload

--- FE_HW13/lib/scripts.js
+++ FE_HW13/lib/scripts.js
@@ -1,15 +1,97 @@
-const ingridients = ["milk", "water", "coffee", "viski", "choklad", "slivki", "sugar"];
-const sugarType = ["0.5", "1.0", "1.5", "2.0", "2.5", "3.0"];
-const cofeeType = [
-    { descr: "Espresso", price: 25, ingridients: [2] },
-    { descr: "Americano", price: 30, ingridients: [2, 1, 1] },
-    { descr: "Capuchino", price: 50, ingridients: [2, 0, 5, 5] },
-    { descr: "Latte", price: 50, ingridients: [2, 0, 5, 5] },
-    { descr: "Mokko", price: 60, ingridients: [4, 2, 0, 5] },
-    { descr: "FlatWhite", price: 60, ingridients: [2, 0] },
-    { descr: "Irish", price: 60, ingridients: [2, 2, 3, 5] },
-    { descr: "Doppio", price: 50, ingridients: [2, 2] }
+const ingridients = [
+    { id: 100, descr: "milk" },
+    { id: 101, descr: "water" },
+    { id: 102, descr: "coffee" },
+    { id: 103, descr: "viski" },
+    { id: 104, descr: "choklad" },
+    { id: 105, descr: "slivki" },
+    { id: 106, descr: "sugar" }
 ];
+
+const sugarType = [
+    { id: 201, descr: "0.5" },
+    { id: 202, descr: "1.0" },
+    { id: 203, descr: "1.5" },
+    { id: 204, descr: "2.0" },
+    { id: 205, descr: "2.5" },
+    { id: 206, descr: "3.0" }
+];
+
+const cofeeType = [
+    { id: 301, descr: "Espresso" },
+    { id: 302, descr: "Americano" },
+    { id: 303, descr: "Capuchino" },
+    { id: 304, descr: "Latte" },
+    { id: 305, descr: "Mokko" },
+    { id: 306, descr: "FlatWhite" },
+    { id: 307, descr: "Irish" },
+    { id: 308, descr: "Doppio" }
+];
+
+const coffeePrice = [
+    { id: 301, price: 25 },
+    { id: 302, price: 30 },
+    { id: 303, price: 50 },
+    { id: 304, price: 50 },
+    { id: 305, price: 60 },
+    { id: 306, price: 60 },
+    { id: 307, price: 60 },
+    { id: 308, price: 50 }
+];
+
+const coffeeIngridients = [
+    { id: 301, ingridients: [102] },
+    { id: 302, ingridients: [102, 101, 101] },
+    { id: 303, ingridients: [102, 100, 105, 105] },
+    { id: 304, ingridients: [102, 100, 105, 105] },
+    { id: 305, ingridients: [104, 102, 100, 105] },
+    { id: 306, ingridients: [102, 100] },
+    { id: 307, ingridients: [102, 102, 103, 105] },
+    { id: 308, ingridients: [102, 102] }
+];
+
+const getDescr = function (id, dataArray) {
+    let descr = '';
+    for (let i=0; i<=dataArray.length; i++){
+        if (dataArray[i].id === id){
+            descr = dataArray[i].descr;
+        }
+    }
+    return descr;
+}
+
+const getIngigient = function (id = 0) {
+    let ingr = '';
+    for (let i=0; i<=ingridients.length; i++){
+        if (ingridients[i].id === id){
+            ingr = ingridients[i].descr;
+            break;
+        }
+    }
+    return ingr;
+}
+
+const getCoffeePrice = function (id = 0) {
+    let price = 0;
+    for (let i=0; i<=coffeePrice.length; i++){
+        if (coffeePrice[i].id === id){
+            price = coffeePrice[i].price;
+            break;
+        }
+    }
+    return price;
+}
+
+const getCoffeeIngridients = function (id = 0) {
+    let ingr = [102];
+    for (let i=0; i<=coffeeIngridients.length; i++){
+        if (coffeeIngridients[i].id === id){
+            ingr = coffeeIngridients[i].ingridients;
+            break;
+        }
+    }
+    return ingr;
+}
 
 var currentButton = '';
 
@@ -34,18 +116,23 @@ for (let rows = 0; rows < cofeeType.length / 2; rows++) {
     const second = rows + (cofeeType.length / 2);
     coffeeButtons += `
             <tr>
-            <td class="tdimg"><img class="smallimg" src="./img/${first + 1}.jpg" alt="${cofeeType[first].descr}"></td>
-            <td class="tdprice">${cofeeType[first].price}<br>${cofeeType[first].descr}</td>
+            <td class="tdimg"><img class="smallimg" src="./img/${cofeeType[first].id}.jpg" alt="${cofeeType[first].descr}"></td>
+            <td class="tdprice">${getCoffeePrice(cofeeType[first].id)}<br>${cofeeType[first].descr}</td>
             <td class="tdbutton">
-                <input type="checkbox" name="button${first}" id="button${first}" class="runcoffe" value="${first}">
+                <input type="checkbox" name="button${first}" id="button${first}" class="runcoffe" value="${cofeeType[first].id}">
                 <label class="runlabel" for="button${first}">RUN</label>
-            </td>
+            </td>`;
+    if (second < cofeeType.length) {
+        coffeeButtons += `
             <td class="tdbutton">
-                <input type="checkbox" name="button${second}" id="button${second}" class="runcoffe" value="${second}">
+                <input type="checkbox" name="button${second}" id="button${second}" class="runcoffe" value="${cofeeType[second].id}">
                 <label class="runlabel" for="button${second}">RUN</label>
             </td>
-            <td class="tdprice">${cofeeType[second].price}<br>${cofeeType[second].descr}</td>
-            <td class="tdimg"><img class="smallimg" src="./img/${second + 1}.jpg" alt="${cofeeType[second].descr}"></td>
+            <td class="tdprice">${getCoffeePrice(cofeeType[second].id)}<br>${cofeeType[second].descr}</td>
+            <td class="tdimg"><img class="smallimg" src="./img/${cofeeType[second].id}.jpg" alt="${cofeeType[second].descr}">
+            </td>`;
+    }
+    coffeeButtons += `
             </tr>`;
 }
 coffeeButtons += `<tr>
@@ -57,8 +144,8 @@ document.querySelector('.machinemenu').innerHTML = coffeeButtons;
 
 let sugarStr = '<legend>Sugar:</legend>';
 for (let i = 0; i < sugarType.length; i++) {
-    sugarStr += `<input type="radio" name="sugar" id="sugar-${i}" value="${sugarType[i]}">
-                    <label for="sugar-${i}">${sugarType[i]}</label>`;
+    sugarStr += `<input type="radio" name="sugar" id="sugar-${i}" value="${sugarType[i].descr}">
+                    <label for="sugar-${i}">${sugarType[i].descr}</label>`;
 }
 
 document.querySelector('.sugar-field').innerHTML = sugarStr;
@@ -71,7 +158,7 @@ let doorsStr = `
                 `;
 
 for (let imgs = 0; imgs < cofeeType.length; imgs++) {
-    doorsStr += `<img class="readyimg img-${imgs}" src="./img/${imgs + 1}.jpg" alt="${cofeeType[imgs].descr}">`;
+    doorsStr += `<img class="readyimg img-${cofeeType[imgs].id}" src="./img/${cofeeType[imgs].id}.jpg" alt="${cofeeType[imgs].descr}">`;
 }
 
 document.querySelector('.tdvisual').innerHTML = doorsStr;
@@ -91,10 +178,11 @@ document.querySelectorAll('.runcoffe').forEach((elem) =>
             //elem.target.checked = false;
             let ingrStr = '';
             const currentItem = parseInt(elem.target.value);
-            const currentCount = cofeeType[currentItem].ingridients.length;
+            const ingr = getCoffeeIngridients(currentItem);
+            const currentCount = ingr.length;
             const ingrStyle = currentCount > 2 ? `height: ${100 / currentCount}%` : `width: 100%`;
             for (let i = currentCount - 1; i >= 0; i--) {
-                ingrStr += `<img class="ingrs" src="./img/${ingridients[cofeeType[currentItem].ingridients[i]]}.jpg" alt="${ingridients[cofeeType[currentItem].ingridients[i]]}" style="${ingrStyle}">`;
+                ingrStr += `<img class="ingrs" src="./img/${getIngigient(ingr[i])}.jpg" alt="${getIngigient(ingr[i])}" style="${ingrStyle}">`;
             }
             document.querySelector(`.ingridients`).innerHTML = ingrStr;
             document.querySelector(`.img-${elem.target.value}`).classList.add("imgtomove");
